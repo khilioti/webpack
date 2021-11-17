18344add689f0e73c156a1c2a504c015cb7ccf82
Nikita Khilioti <n.khilioti@gmail.com>
2021-11-17 16:45:19 +0200

    Initial commit

---
+++ src/analitics.js
@@ -0,0 +1,23 @@
+function createAnalitics() {
+   let counter = 0
+   let isDestroyed = false
+
+    const listener = () => counter++
+
+    document.addEventListener('click', listener)
+
+    return {
+       destroy() {
+           document.removeEventListener('click', listener)
+         isDestroyed = true
+       },
+       getClicks() {
+           if (isDestroyed) {
+               return 'Analitics is destroyed'
+           }
+           return counter
+       }
+    }
+}
+
+window.analitics = createAnalitics()
